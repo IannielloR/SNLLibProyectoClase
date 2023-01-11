c80ff536426dd18fd451aae932a8d86df6bffc45
IannielloR <86430349+IannielloR@users.noreply.github.com>
2023-01-11 14:53:57 -0300

    Update BicicletaMountainBike.cs

--- LibProyectoClase/Models/Terrestres/Bicicletas/BicicletaMountainBike.cs
+++ LibProyectoClase/Models/Terrestres/Bicicletas/BicicletaMountainBike.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace LibProyectoClase.Models.Terrestre.Bici
 {
-    public class BicicletaMountainBike:Tierra
+    public class BicicletaMountainBike:Bicicleta
     {
         public BicicletaMountainBike(int ruedas, int capacidad, bool guardaBarros, string peso, string velocidad, bool suspension) : base(ruedas, capacidad, guardaBarros, peso)
         {
