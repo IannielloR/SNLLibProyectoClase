47243f66142430c2d6a9dd408fe46167e15b831a
IannielloR <86430349+IannielloR@users.noreply.github.com>
2023-01-11 14:55:22 -0300

    Update AutoUrbano.cs

--- LibProyectoClase/Models/Terrestres/Autos/AutoUrbano.cs
+++ LibProyectoClase/Models/Terrestres/Autos/AutoUrbano.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace LibProyectoClase.Models.Terrestre.Auto
 {
-    public class AutoUrbano:Tierra
+    public class AutoUrbano:Auto
     {
         public AutoUrbano(int ruedas, int capacidad, string color, string marca, string usado) : base(ruedas, capacidad, color)
         {
